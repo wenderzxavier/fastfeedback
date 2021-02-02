a4dd625eab69aa2362f8047871e92040c6205492
Wender Z. Xavier <wender.zacariasxavier@gmail.com>
2021-02-02 15:29:18 -0300

    add chakraui and integration to firebase

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,19 @@
-import '../styles/globals.css';
-import { ProviderAuth } from '../lib/auth';
+import React from 'react';
+import { AuthProvider } from '@/lib/auth';
+import { ChakraProvider, CSSReset, extendTheme } from '@chakra-ui/react';
+import customTheme from '@/styles/theme';
 
-function MyApp({ Component, pageProps }) {
+const theme = extendTheme({ customTheme });
+
+const App = ({ Component, pageProps }) => {
   return (
-    <ProviderAuth>
-      <Component {...pageProps} />;
-    </ProviderAuth>
+    <ChakraProvider theme={theme}>
+      <CSSReset />
+      <AuthProvider>
+        <Component {...pageProps} />;
+      </AuthProvider>
+    </ChakraProvider>
   );
-}
+};
 
-export default MyApp;
+export default App;
--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,27 @@
 import Head from 'next/head';
-import { useAuth } from '../lib/auth';
-import styles from '../styles/Home.module.css';
+import { Button, Code, Heading, Text } from '@chakra-ui/react';
+
+import { useAuth } from '@/lib/auth';
 
 export default function Home() {
   const auth = useAuth();
 
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Fast Feedback</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-        <button onClick={(e) => auth.signinWithGithub()}>Sign in</button>
-        <div>{auth?.user?.email}</div>
-        {auth?.user && (
-          <button onClick={(e) => auth.signout()}>Sign Out</button>
+      <main>
+        <Heading>Fast Feedback</Heading>
+        <Text>
+          Current user: <Code>{auth.user ? auth.user.email : 'None'}</Code>
+        </Text>
+        {auth.user ? (
+          <Button onClick={(e) => auth.signout()}>Sign Out</Button>
+        ) : (
+          <Button onClick={(e) => auth.signinWithGithub()}>Sign In</Button>
         )}
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      </main>{' '}
     </div>
   );
 }
