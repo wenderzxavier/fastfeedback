6ef3c5bbacfa8552e6567bd39c1357b19dbf3c8a
Wender Z. Xavier <wender.zacariasxavier@gmail.com>
2021-02-04 15:53:49 -0300

    Add Firebase Admin, SWR and fetch all sites

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,14 +1,29 @@
-import { useAuth } from '@/lib/auth';
+import useSWR from 'swr';
+
+import DashboardShell from '@/components/DashboardShell';
 import EmptyState from '@/components/EmptyState';
+import SiteTableSkeleton from '@/components/SiteTableSkeleton';
+import SiteTable from '@/components/SiteTable';
+import fetcher from '@/utils/fetcher';
 
 const Dashboard = () => {
-  const auth = useAuth();
+  const { data } = useSWR('/api/sites', fetcher);
 
-  if (!auth.user) {
-    return 'Loading...';
+  if (!data) {
+    return (
+      <DashboardShell>
+        <SiteTableSkeleton />
+      </DashboardShell>
+    );
   }
 
-  return <EmptyState />;
+  console.log(data);
+  
+  return (
+    <DashboardShell>
+      {data?.sites ? <SiteTable sites={data.sites} /> : <EmptyState />}
+    </DashboardShell>
+  );
 };
 
 export default Dashboard;
